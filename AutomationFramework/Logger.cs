be3400adb3e358a98090b3348ee6e67109bdb77c
nicopiko <nstall22@student.aau.dk>
2025-05-22 12:49:45 +0200

    Added functionality to Logger.cs

--- AutomationFramework/Logger.cs
+++ AutomationFramework/Logger.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace AutomationFramework
 {
-    class Logger
+    public class Logger
     {
+        private readonly string logPath;
+
+        public Logger(string moduleName)
+        {
+            string moduleDir = Path.Combine("Logs", moduleName);
+            Directory.CreateDirectory(moduleDir);
+
+            logPath = Path.Combine(moduleDir, $"{moduleName}_logs");
+        }
+
+        public void Log(string message)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string logEntry = $"[{timestamp}] {message}";
+
+            File.AppendAllText(logPath, logEntry + Environment.NewLine);
+        }
     }
 }
